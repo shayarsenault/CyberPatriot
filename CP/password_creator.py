cec1c129aeb9be2674b19ab35a4b22511091e081
shayarsenault <shayarsenault@yahoo.com>
2017-11-18 13:27:08 -0500

    Changes

--- CP/password_creator.py
+++ CP/password_creator.py
@@ -1,14 +1,15 @@
 from random import *
 import os
 
-chars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0','!','@','#','$']
+chars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 
+'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0','!','@','#','$','%','_','+','.',',','|','[',']']
 
 users = []
 pwlist = []
 
 pwl = 12
 i = 0
-rpw = input('enter RPW')
+#rpw = input('enter RPW')
 
 #pwlist.append(pw)
 #print(pw)
@@ -29,13 +30,13 @@ while 1:
 		for i in range(len(users)):
 			f.write('%s: ' % users[i])
 			f.write('%s\n\n' % pwlist[i])
-			echo_pw()
+			
 		f.close()
 		
 		exit()
 	else:
 		while len(pw) <= pwl:
-			pw += pw.join(x for x in chars[randrange(60)])
+			pw += pw.join(x for x in chars[randrange(64)])
 		
 		pwlist.append(pw)
 		users.append(user)
