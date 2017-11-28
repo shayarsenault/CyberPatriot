de0351c52e20a34c914b40440c5279d2ca9402a2
shayarsenault <shayarsenault@yahoo.com>
2017-11-28 18:13:52 -0500

    Specific directory input added

--- CP/find_all.py
+++ CP/find_all.py
@@ -1,9 +1,9 @@
 import os
 
-
+directory = input('Enter /home/ if on Ubuntu, or /Users/ if on Windows or OS X: ')
 ext = input('Enter extension to look for. i.e .jpeg: ')
 
-for root, dirs, files in os.walk('/'):
+for root, dirs, files in os.walk(directory):
 	for file in files:
 		if file.endswith(ext):
 			print(os.path.join(root, file))
