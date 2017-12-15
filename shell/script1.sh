61407325a57e37407cc1ca10ea13f240083f6c77
shayarsenault <shayarsenault@yahoo.com>
2017-12-14 20:50:48 -0500

    Initial commit

--- shell/script1.sh
+++ shell/script1.sh
@@ -1,2 +1,36 @@
 #!/bin/bash
-echo "hello world"
+clear
+echo GLHF
+echo
+echo
+echo
+echo
+main(){
+  echo Enter password for root
+  read pass
+  echo installing expect if needed
+  sudo apt-get install Expect
+  expect -c "
+    expect "password:"
+    send "$pass\r"
+  "
+  clear
+  echo Done.
+  echo
+  echo
+  echo Enabling UFW
+  sudo ufw enable
+  expect -c "
+    expect "password:"
+    send "$pass\r"
+  "
+  clear
+  echo Done
+  echo
+  echo
+  echo
+  echo Finding extensions
+  find / -name '.jpg'
+}
+
+main
